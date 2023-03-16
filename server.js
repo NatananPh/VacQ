e8b679289b09e0950a0ab1eb60d12e22ff8eb612
NatananPh <nut14586@gmail.com>
2023-03-16 18:36:40 +0700

    ✨Add appointment

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ const auth = require('./routes/auth');
 app.use('/api/v1/hospitals', hospitals);
 app.use('/api/v1/auth', auth);
 
+const appointments = require('./routes/appointments');
+app.use('/api/v1/appointments', appointments);
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
